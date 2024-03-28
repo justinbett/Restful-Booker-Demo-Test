334c2e5e9e9b8b185b0566f224ec4cae06c82bce
Lex JVB <justinvbettencourt@gmail.com>
2024-03-28 10:06:46 -0400

    Initial Push
    Initial push for the Restful Booker demo test.

---
+++ Workspace/restful-booker-demo/src/main/java/com/justinbett/restful_booker_demo/App.java
@@ -0,0 +1,13 @@
+package com.justinbett.restful_booker_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Workspace/restful-booker-demo/src/test/java/com/justinbett/restful_booker_demo/AppTest.java
@@ -0,0 +1,20 @@
+package com.justinbett.restful_booker_demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
