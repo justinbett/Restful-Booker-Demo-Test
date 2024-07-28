91ade70806ec209bbfcc0aabde8d990491f1860f
Justin B <justinvbettencourt@gmail.com>
2024-07-28 15:03:16 -0400

    Test fixes

--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -6,20 +6,21 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 @Listeners(org.example.resources.Listeners.class)
-public class HelloWorldTest extends Hooks {
+public class PracticeTest extends Hooks {
 
-    public HelloWorldTest() throws IOException {
+    public PracticeTest() throws IOException {
         super();
     }
 
     @Test
-    public static void HelloWorldTest1() throws IOException {
-        HomePage homePage = new HomePage();
-
+    public static void HelloWorldTest() {
         System.out.println("Hello World!");
+    }
 
+    @Test
+    public static void ClickTest() throws IOException {
+        HomePage homePage = new HomePage();
         homePage.getBookRoom1().click();
-
         System.out.println("Book Room 1: Click");
     }
 
